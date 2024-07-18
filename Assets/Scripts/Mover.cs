802459962fd2f7073a7db7f840572f356b36f913
GrigoryEbl <yatrahal.rus@mail.ru>
2024-07-18 20:09:53 +0400

    Refact mover

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,16 +1,13 @@
 using UnityEngine;
 
-[RequireComponent (typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody))]
 public class Mover : MonoBehaviour
 {
-   [SerializeField] private float _speed = 4f;
+    [SerializeField] private float _speed = 4f;
 
     private Transform _transform;
     private Rigidbody _rigidbody;
 
-    public float Speed => _speed;
-    public float CurrentSpeed { get; private set; }
-    public int Level { get; private set; }
     public bool IsMoving { get; private set; }
 
     private void Awake()
@@ -24,7 +21,6 @@ public class Mover : MonoBehaviour
         _transform.LookAt(_transform.position + direction);
         _rigidbody.velocity = direction * _speed;
 
-        CurrentSpeed = _rigidbody.velocity.magnitude;
         IsMoving = true;
     }
 
